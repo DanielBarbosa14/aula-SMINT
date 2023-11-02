22ff690c702a6af91cbcf0d7390aff1b632a4645
DanielBarbosa14 <daniel.alves.barbosa@ipvc.pt>
2023-11-02 20:31:38 +0000

    a

---
+++ photo-gallery/src/pages/Tab2.tsx
@@ -0,0 +1,48 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet,
+} from '@ionic/react';
+
+const Tab2: React.FC = () => {
+  function takePhoto(): void {
+    throw new Error('Function not implemented.');
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab 2</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Photo Gallery</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="Tab 2 page" />
+        <IonContent>
+  <IonFab vertical="bottom" horizontal="center" slot="fixed">
+    <IonFabButton onClick={() => takePhoto()}>
+      <IonIcon icon={camera}></IonIcon>
+    </IonFabButton>
+  </IonFab>
+</IonContent>
+</IonContent>
+    </IonPage>
+
+  );
+};
+
+export default Tab2;
